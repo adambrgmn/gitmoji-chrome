35e03659a909e0f1a55f949186c6615a33b494b8
Adam Bergman <adam@fransvilhelm.com>
2017-10-26 11:02:45 +0200

    :sparkles: Add Link component

---
+++ src/components/Link/index.js
@@ -0,0 +1,28 @@
+/* global chrome */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Link extends Component {
+  static propTypes = {
+    href: PropTypes.string.isRequired,
+    className: PropTypes.string,
+    children: PropTypes.node.isRequired,
+  };
+
+  handleClick = e => {
+    if (chrome && 'tabs' in chrome) {
+      e.preventDefault();
+      chrome.tabs.create({ url: this.props.href });
+    }
+  };
+
+  render() {
+    const { href, className, children } = this.props;
+
+    return (
+      <a href={href} className={className} onClick={this.handleClick}>
+        {children}
+      </a>
+    );
+  }
+}
