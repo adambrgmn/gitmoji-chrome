89ec60d7180ad18bc8bd6b054a3186ac8dd90408
Adam Bergman <adam@fransvilhelm.com>
2017-11-10 12:38:38 +0100

    :zap: Prevent unnesecary rerender

--- src/components/RecentList/RecentPreview.js
+++ src/components/RecentList/RecentPreview.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import * as types from '../../propTypes';
 
-function RecentPreview({ emoji, onClick }) {
-  return (
-    <button
-      className="recent-button"
-      style={{ backgroundColor: emoji.color }}
-      onClick={onClick}
-    >
-      <span className="recent-emoji">{emoji.emoji}</span>
-    </button>
-  );
-}
+class RecentPreview extends Component {
+  static propTypes = {
+    emoji: types.emoji.isRequired,
+    onClick: PropTypes.func.isRequired,
+  };
 
-RecentPreview.propTypes = {
-  emoji: types.emoji,
-  onClick: PropTypes.func.isRequired,
-};
+  shouldComponentUpdate(nextProps) {
+    return this.props.emoji.code !== nextProps.emoji.code;
+  }
+
+  render() {
+    const { emoji, onClick } = this.props;
+    console.log('rerender', emoji.emoji);
+    return (
+      <button
+        className="recent-button"
+        style={{ backgroundColor: emoji.color }}
+        onClick={onClick}
+      >
+        <span className="recent-emoji">{emoji.emoji}</span>
+      </button>
+    );
+  }
+}
 
 export { RecentPreview as default };
