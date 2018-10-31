adc42a2e476944cf53d622e7e9e215fda81800e8
Adam Bergman <adam@fransvilhelm.com>
2018-10-31 08:31:08 +0100

    chore: Clear away all old code

--- src/store/recent/constants.js
+++
@@ -1,3 +0,0 @@
-const RECENT_ADD = 'RECENT_ADD';
-
-export { RECENT_ADD };
--- src/store/settings/actions.js
+++
@@ -1,5 +0,0 @@
-import { SETTINGS_TOGGLE_VISIBILITY } from './constants';
-
-const toggleSettings = () => ({ type: SETTINGS_TOGGLE_VISIBILITY });
-
-export { toggleSettings };
