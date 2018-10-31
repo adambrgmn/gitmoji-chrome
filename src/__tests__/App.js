8f0746e63bd5fb70a13ad24d38e92d3c2422e220
Adam Bergman <adam@fransvilhelm.com>
2018-10-31 11:59:30 +0100

    chore: Clear up code before writing actual app

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,19 +1,7 @@
 import React from 'react';
-import { render, waitForElement, fireEvent } from 'react-testing-library';
+import { render } from 'react-testing-library';
 import App from '../App';
 
-it('renders two buttons and current count', async () => {
-  const { getByText } = render(<App />);
-  await waitForElement(() => getByText(/\+/));
-
-  const increment = getByText(/\+/);
-  const decrement = getByText(/-/);
-
-  expect(getByText(/\d/)).toHaveTextContent(0);
-
-  fireEvent.click(increment);
-  await waitForElement(() => getByText('1'));
-
-  fireEvent.click(decrement);
-  await waitForElement(() => getByText('0'));
+it('should render without problem', async () => {
+  render(<App />);
 });
