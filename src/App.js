a6a12f346bd298d906accc0f0645bf6eaf7f501a
Adam Bergman <adam@fransvilhelm.com>
2018-10-31 11:59:30 +0100

    chore: Clear up code before writing actual app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import React, { Suspense, lazy, useState } from 'react';
-
-const Button = lazy(() => import('./Button'));
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const change = val => async () => {
-    await Promise.resolve();
-    setCount(c => c + val);
-  };
-
-  return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <div>
-        <Button onClick={change(-1)}>-</Button>
-        <span>{count}</span>
-        <Button onClick={change(+1)}>+</Button>
-      </div>
-    </Suspense>
-  );
+  return <div />;
 }
 
 export default App;
