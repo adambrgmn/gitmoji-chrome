8a902b27c45c916404c58b6341c175d95a650caa
Adam Bergman <adam@fransvilhelm.com>
2018-10-31 11:54:20 +0100

    style: Adhere to eslint rules

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Button({ children, ...props }) {
-  return <button {...props}>{children}</button>;
+  return (
+    <button type="button" {...props}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
