7d9dc91176f8815f8026717f584360d23f30411c
Adam Bergman <adam@fransvilhelm.com>
2018-11-02 14:46:01 +0100

    revert: Remove ConcurrentMode and unstabe render method
    The ConcurrentMode and the createRoot().render-method caused some confusion. It didn't respond to
    Suspense the way I thought and I will need to do some more research before using it. This will
    probably be clearer when the React them develops the feature further.

--- src/popup.js
+++ src/popup.js
@@ -1,9 +1,5 @@
-import React, { ConcurrentMode } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <ConcurrentMode>
-    <App />
-  </ConcurrentMode>,
-);
+ReactDOM.render(<App />, document.getElementById('root'));
